341f1c25068376dcd3fb6b67e56db2cd3bdc3041
rubenlorenzo98 <rubenlorenzo998@gmail.com>
2022-05-17 23:24:25 +0200

    initial commit

---
+++ src/main/java/com/test/superheromaintenance/SuperheroMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.test.superheromaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroMaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/superheromaintenance/SuperheroMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.superheromaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
